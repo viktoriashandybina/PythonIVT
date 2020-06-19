0534eab43fd39163b731c76d7db75491ed01d16b
viktoriashandybina <32095499+viktoriashandybina@users.noreply.github.com>
2020-06-19 21:02:05 +0300

    Add files via upload

---
+++ 4sem/4. Встроенные коллекции/ВСР.py
@@ -0,0 +1,18 @@
+from random import randint
+
+def sort(nums):  
+    for i in range(1, len(nums)):
+        k = nums[i]
+        j = i - 1
+        while j >= 0 and nums[j] > k:
+            nums[j + 1] = nums[j]
+            j -= 1
+        nums[j + 1] = k
+
+
+z = []
+for i in range(10):
+    z.append(randint(-10, 10))
+sort(z)
+print('метод вставки')  
+print(z)  
--- 4sem/4. Встроенные коллекции/ИСР.py
+++ 4sem/4. Встроенные коллекции/ИСР.py
@@ -1,5 +1,3 @@
-import numpy as np
-import random
 from random import randint
 
 def sort(nums):  
